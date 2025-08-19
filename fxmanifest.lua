92b68b0be29f39d956adb30a4b81e927530b41cd
Arta_SD <artanorani@gmail.com>
2025-08-19 20:14:15 +0330

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+a 'arta_zx'
+b 'remove nps'
+c '1.0.0'
+
+dependency 'es_extended'
+
+client_scripts {
+
+    'client.lua'
+}
+
+server_scripts {
+    '@es_extended/locale.lua',
+    'server.lua'
+}
